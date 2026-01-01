495716c75e934ad302bae19767948156b56b1791
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2026-01-02 02:45:16 +0530

    Clean up review routes structure

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -9,13 +9,27 @@ const {
   deleteReviewAdmin
 } = require("../controllers/reviewController");
 
-// Customer
+/**
+ * CUSTOMER ROUTES
+ */
+
+// Add new review
 router.post("/", addReview);
+
+// Get approved reviews
 router.get("/", getReviews);
 
-// Admin
+/**
+ * ADMIN ROUTES
+ */
+
+// Get all reviews (admin)
 router.get("/admin", getAllReviewsAdmin);
+
+// Update review (admin)
 router.put("/admin/:id", updateReviewAdmin);
+
+// Delete review (admin)
 router.delete("/admin/:id", deleteReviewAdmin);
 
 module.exports = router;
